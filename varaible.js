cd6ea8be3c04c2883fbbb9d95a60bfe93cffe4a8
Adarsh Jha <112857989+Adarshjha0007@users.noreply.github.com>
2024-04-12 23:02:17 +0530

    Add files via upload

---
+++ varaible.js
@@ -0,0 +1,13 @@
+//string
+var name = "Adarsh";
+var lastName = "Jha"
+//number
+var age = 21;
+var moblieNumber = 7461032776;
+//boolean
+var following = true;
+var following = false;
+//null
+ var a = null;
+//undefined
+ var b 
