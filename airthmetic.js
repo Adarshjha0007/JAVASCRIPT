ed19f9a38b37c080b11c4136edbe601deef4ac9a
ADARSH KUMAR <112857989+Adarshjha0007@users.noreply.github.com>
2024-04-14 22:42:54 +0530

    Add files via upload

---
+++ airthmetic.js
@@ -0,0 +1,13 @@
+// Airthmetic  operators
+let a = 5;
+let b = 2;
+
+console.log("a = ", a);
+console.log("b = ", b);
+
+ console.log("a + b = ", a+b); //plus
+ console.log("a - b = ", a-b); //minus
+ console.log("a * b = ", a*b); //multiplication
+ console.log("a / b = ", a/b); //division
+ console.log("a % b = ", a%b); //modulus
+ console.log("a ** b = ", a**b); //exponential
